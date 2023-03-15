521e0a6350ae2821073cb5f683a11d8744c9d7a1
Jakub <kubulany@gmail.com>
2023-03-15 22:21:40 +0100

    Name change

--- flower-shop/src/test/java/com/flower/shop/NewsletterTest.java
+++ flower-shop/src/test/java/com/flower/shop/NewsletterTest.java
@@ -3,7 +3,7 @@ package com.flower.shop;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
-public class ClientSubscribingResignigNewsletterTest {
+public class NewsletterTest {
     @Test
     public void shouldReturnBadRequestBecauseBodyIsInvalid() {
         HttpStatus result = HttpStatus.BAD_REQUEST;
