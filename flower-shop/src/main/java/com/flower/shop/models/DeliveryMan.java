4d23d7e2f6cf1aaf8310ec50accf774ebf58364c
MateuszDobron <mateuszdobron@gmail.com>
2023-03-22 21:14:29 +0100

    first

---
+++ flower-shop/src/main/java/com/flower/shop/models/DeliveryMan.java
@@ -0,0 +1,20 @@
+package com.flower.shop.models;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "DeliveryMan")
+@Data
+public class DeliveryMan {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    private UUID productId;
+}
+
