96f7fb1f636d1a4fd12b4635e0f1ea7f671a266b
Piotr Mozeluk <piotr.mozeluk@gmail.com>
2023-04-02 22:26:17 +0200

    implements UserDetails to model and adding new dependency

--- flower-shop/src/main/java/com/flower/shop/data/models/Person.java
+++ flower-shop/src/main/java/com/flower/shop/data/models/Person.java
@@ -1,15 +1,20 @@
 package com.flower.shop.data.models;
 import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.UUID;
 
 @Entity
 @Table(name = "persons")
 @Data
 @Inheritance(strategy = InheritanceType.JOINED)
-public class Person {
+public class Person implements UserDetails {
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(
@@ -30,4 +35,47 @@ public class Person {
     @Column
     private String password;
 
+    @Column
+    private boolean accountNonLocked;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return emailAddress;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public void setAccountNonLocked(Boolean accountNonLocked) {
+        this.accountNonLocked = accountNonLocked;
+    }
 }
