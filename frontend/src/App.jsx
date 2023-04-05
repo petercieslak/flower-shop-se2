8d6b4364f2edc2407464c2971d1994b7c20bc3ab
Jakub <jakubbodak1@gmail.com>
2023-04-06 00:19:44 +0200

    update to view navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import ProductsPage from "./Pages/ProductsPage";
 import LoginPage from "./Pages/LoginPage";
 import HomePage from "./Pages/HomePage";
 import Navbar from "./Pages/Navbar";
+
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,8 +10,8 @@ function App() {
     <>
       {/* <Navbar /> */}
       <Routes>
-        <Route path="/products" element={<ProductsPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/products" element={<><Navbar /><ProductsPage /></>} />
+        <Route path="/login" element={<><Navbar /><LoginPage /></>} />
         <Route path="/" element={<HomePage />} />
       </Routes>
     </>
