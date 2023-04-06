a3f2d581838939a2b6a4f25b77c1c89d72954270
simonsrz <szymonslusarz01@gmail.com>
2023-04-06 09:10:08 +0200

    unnecessary files removed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -11,8 +11,7 @@ export default {
       },
       backgroundImage: {
         'leaves-pattern': "url('./src/assets/leaves-bg.png')",
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'test': "url('./src/assets/test.jpg')",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))'
       },
 
     },
