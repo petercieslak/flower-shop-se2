fc207e5e884e13727cb0b03850f5e38028c9e171
Piotr Mozeluk <piotr.mozeluk@gmail.com>
2023-04-19 20:28:19 +0200

    adding admin panel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import ProductsPage from "./Pages/ProductsPage";
 import LoginPage from "./Pages/LoginPage";
 import HomePage from "./Pages/HomePage";
 import Navbar from "./Pages/Navbar";
+import AdminPage from "./Pages/AdminPage";
 
 import { Routes, Route } from "react-router-dom";
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/products" element={<><Navbar /><ProductsPage /></>} />
         <Route path="/login" element={<><Navbar /><LoginPage /></>} />
         <Route path="/" element={<HomePage />} />
+        <Route path="/admin" element={<AdminPage />} />
       </Routes>
     </>
   );
