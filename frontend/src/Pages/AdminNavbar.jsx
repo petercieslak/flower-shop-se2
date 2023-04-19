b82891d13f17734b5cca06c31b15e48573da2343
simonsrz <szymonslusarz01@gmail.com>
2023-04-19 22:33:51 +0200

    added simple add form

--- frontend/src/Pages/AdminNavbar.jsx
+++ frontend/src/Pages/AdminNavbar.jsx
@@ -1,19 +1,24 @@
-
 function AdminNavbar() {
-    return (
-        <nav className="bg-white shadow-lg">
-            <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-                <div className="flex justify-between h-16">
-                    <div className="flex">
-                        <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
-                            <a href="#" className="px-3 py-5 text-gray-900 font-medium hover:text-gray-700">Products</a>
-                            <a href="#" className="px-3 py-5 text-gray-900 font-medium hover:text-gray-700">Users</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="bg-white shadow-lg fixed top-0 w-screen">
+      <div className="flex">
+        <div className="ml-6 flex space-x-8">
+          <a
+            href="#"
+            className="px-3 py-5 text-gray-900 font-medium hover:text-gray-700"
+          >
+            Products
+          </a>
+          <a
+            href="#"
+            className="px-3 py-5 text-gray-900 font-medium hover:text-gray-700"
+          >
+            Users
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default AdminNavbar;
